e86e68a5afb98f053ec42902879c8c49823c7f66
Ghazanfar Shah Uzair <59178655+gsuzair@users.noreply.github.com>
2025-10-03 23:42:14 +0200

    padding

--- client/src/screens/Shop/Shop.jsx
+++ client/src/screens/Shop/Shop.jsx
@@ -5,9 +5,9 @@ import CardSkeleton from '../../components/Layout/Product/CardSkeleton';
 const Shop = () => {
 
   return (
-    <div className="coontainer">
+    <div className="">
       <Header />
-        <div className="max-w-7xl mx-auto px-3 sm:px-5 lg:px-6">
+        <div className="max-w-7xl mx-auto px-3 py-3 sm:px-5 lg:px-6">
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {Array.from({ length: 12 }).map((_, i) => <CardSkeleton key={i} />)}
             </div>
