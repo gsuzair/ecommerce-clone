b18ad42da13f9a5a733cca8ad4cfe66f8ad8d329
Ghazanfar Shah Uzair <59178655+gsuzair@users.noreply.github.com>
2025-10-05 19:58:14 +0200

    fixes

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -7,6 +7,12 @@ import Feature from "../screens/Features/Feature";
 import HomePage from "../screens/Home/HomePage";
 import Shop from "../screens/Shop/Shop";
 import Signup from "../screens/Signup/SignUp";
+import Dashboard from '../screens/Admin/Dashboard/Dashboard';
+import Invoice from '../screens/Admin/Invoice/Invoice';
+import Billing from '../screens/Admin/Billing/Billing';
+import Order from '../screens/Admin/Order/Order';
+import Product from '../screens/Admin/Product/Product';
+import User from '../screens/Admin/User/User';
 
 export default function AppRoutes() {
   return (
